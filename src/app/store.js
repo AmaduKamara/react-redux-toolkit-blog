1cc8b9f343d22169a9e52db1b6164a8201e9aa44
Amkam <amaduskamara@gmail.com>
2022-06-19 23:56:27 +0000

    Edit posts directory title and import postsReducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { postReducer } from "../features/posts/postsSlice";
 
 export const store = configureStore({
-  reducer: {
-    
-  }
-})
+  reducer: { posts: postReducer },
+});
