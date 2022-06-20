a28399bb2c41767d2dc4f404857d28f7fb811c77
Amkam <amaduskamara@gmail.com>
2022-06-20 13:49:04 +0000

    Import TimeAgo component in PostList, render it and parse the itmestamp and date prop to it

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -11,6 +12,7 @@ const PostsList = () => {
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
