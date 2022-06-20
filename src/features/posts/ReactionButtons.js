ccd566ccff43eb679995b8d3c55dff5a2bf6cb20
Amkam <amaduskamara@gmail.com>
2022-06-20 14:54:45 +0000

    Add ReactionButton component

---
+++ src/features/posts/ReactionButtons.js
@@ -0,0 +1,33 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const reactionEmoji = {
+  thumbsUp: "👍",
+  wow: "😯",
+  heart: "💖",
+  rocket: "🚀",
+  coffee: "☕",
+};
+
+const ReactionButtons = ({ post }) => {
+  const dispatch = useDispatch();
+
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <button
+        type="button"
+        key={name}
+        className="reactionButton"
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+
+  return <div>{reactionButtons}</div>;
+};
+
+export default ReactionButtons;
