906e2ff639cfe3aecefcbc69fbd99b85830752fe
Amkam <amaduskamara@gmail.com>
2022-06-20 13:45:26 +0000

    add date-fns library for displaying date and created a TimeAgo component to parse the date

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
@@ -6,11 +7,13 @@ const initialState = [
     title: "Learning Redux Toolkit",
     content:
       "I need to know Redux for MERN app development and state management",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: "2",
     title: "Learning the MERN Stack",
     content: "I need to know the MERN Stack",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
 ];
 
@@ -24,7 +27,13 @@ const postsSlice = createSlice({
       },
       prepare(title, content, userId) {
         return {
-          payload: { id: nanoid(), title, content, userId },
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+          },
         };
       },
     },
